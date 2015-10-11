5cc9b69f8b12926c442df4ee63edc567f8bc3067
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2015-10-10 23:21:16 -0500

    Migraciones y seeders con faker, config bd

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -6,7 +6,7 @@ class UserTableSeeder extends Seeder {
 
 	public function run() {
 		DB::table('users')->delete();
-		
+
 		\DB::table('users')->insert(array (
 			'profile' => 'sp_admin',
 			'name' => 'Alexander Londono',
@@ -14,5 +14,17 @@ class UserTableSeeder extends Seeder {
 			'enable' => 'si',
 			'password' => \Hash::make('admin')
 			));
+
+		$faker = Faker\Factory::create();
+        $count = 50;
+        foreach (range(1, $count) as $index) {
+		\DB::table('users')->insert(array (
+			'profile' => 'usuario',
+			'name' => $faker->lastName,
+			'email' => $faker->email,
+			'enable' => 'si',
+			'password' => \Hash::make('12345')
+			));
+		}
 	}
 }

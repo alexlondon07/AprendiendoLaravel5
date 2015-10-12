2a3611e750c13bb8709a4244e967f7ea1d5619be
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2015-10-12 12:53:46 -0500

    se integran librerias
    de boostrap , uyil, css, jQuery, se lista usuarios

--- resources/views/admin/user/view_user.blade.php
+++ resources/views/admin/user/view_user.blade.php
@@ -1,52 +1,67 @@
+@extends('template.generic_admin')
 
+@section('head_content')
+@stop
+
+@section('body_content')
 <div class="container-fluid">
     <div class="row">
         <div class="main">
-            <h1 class="page-header">Usuarios</h1>
+            <h2 class="page-header">Usuarios</h2>
             <div class="controls form-inline">
 
-                <a href="{!!URL::to('/') !!}/admin/user/create" class="btn btn-primary pull-right">Ingresar usuarios</a>
-
+                <a href="{!! URL::to('/') !!}/admin/user/create" class="btn btn-primary pull-right">Crear Usuario</a>
                 <div class="input-group">
                   {!! Form::open(array('url' => 'admin/users/search', 'id' => 'search_form', 'method'=>'GET', 'class'=>'control-group')) !!}
-
+                  <div class="form-group">
+                        <input id="search"  name="search"  type="text" required="true" class="form-control" placeholder="Buscar..." value="@if(isset($search)){{ $search }}@endif" >
+                  </div>
+                  <button class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                  <a href="{!!URL::to('/')!!}/admin/user" title="Refrescar Usuarios"class="btn btn-default"><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span></a>
                   {!! Form::close() !!}
               </div>
-          </div>
-            <div class="table-responsive">
-                @if (count($items) > 0)
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>&nbsp;</th>
-                            <th>Nombre</th>
-                            <th>Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($items as $item)
-                        <tr>
-                            <td style="width: 150px !important">
-                                <table>
-                                    <tr>
-                                        <td>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </td>
-                            <td>{!! $item->name !!}</td>
-                            <td>{!! $item->email !!}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-
-                <nav class="text-center">
-                </nav>
-                @else
-                No hay datos!
-                @endif
             </div>
-        </div>
+          <div class="table-responsive">
+            @if (count($items) > 0)
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Acciones</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                        <th>Habilitado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($items as $item)
+                    <tr>
+                        <td style="width: 150px !important">
+                            <table>
+                              <tr>
+                                <td><a title="Detalles" href="{!! URL::to('/') !!}/admin/user/{!! $item->id !!}"><span class="glyphicon glyphicon-eye-open btn btn-default btn-xs"></span></a></td>
+                                <td><a title="Editar" href="{!! URL::to('/') !!}/admin/user/{{ $item->id !!}/edit"><span class="glyphicon glyphicon-edit btn btn-default btn-xs"></span></a></td>
+                                <td>{!! Form::open(['action' => ['UserController@destroy', $item->id], 'method' => 'delete', 'style' => 'display: inline;']) !!}
+                                    <button title="Eliminar" type="submit" onclick="return Util.confirmDelete(this);" class="glyphicon glyphicon-trash btn btn-default btn-xs"></button>
+                                    {!! Form::close() !!}
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td>{!! $item->name !!}</td>
+                    <td>{!! $item->email !!}</td>
+                    <td>{!! $item->enable !!}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @else
+        No hay datos!
+        @endif
     </div>
 </div>
+</div>
+</div>
+@stop
+
+
+

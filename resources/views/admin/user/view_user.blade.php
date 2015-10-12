3ffd647e8759c2187cb5816cf05eacf674b81c02
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2015-10-11 23:59:47 -0500

    Se muestra usuarios
    se instala "laravelcollective/html": "~5.0"

--- resources/views/admin/user/view_user.blade.php
+++ resources/views/admin/user/view_user.blade.php
@@ -1,10 +1,26 @@
-  <table class="table table-striped">
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="main">
+            <h1 class="page-header">Usuarios</h1>
+            <div class="controls form-inline">
+
+                <a href="{!!URL::to('/') !!}/admin/user/create" class="btn btn-primary pull-right">Ingresar usuarios</a>
+
+                <div class="input-group">
+                  {!! Form::open(array('url' => 'admin/users/search', 'id' => 'search_form', 'method'=>'GET', 'class'=>'control-group')) !!}
+
+                  {!! Form::close() !!}
+              </div>
+          </div>
+            <div class="table-responsive">
+                @if (count($items) > 0)
+                <table class="table table-striped">
                     <thead>
                         <tr>
                             <th>&nbsp;</th>
                             <th>Nombre</th>
-                            <th>Usuario</th>
-                            <th>Habilitado</th>
+                            <th>Email</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -13,14 +29,24 @@
                             <td style="width: 150px !important">
                                 <table>
                                     <tr>
-       
+                                        <td>
+                                        </td>
                                     </tr>
                                 </table>
                             </td>
-                            <td>{{ $item->name }}</td>
-                            <td>{{ $item->email }}</td>
-                            <td>{{ $item->enable }}</td>
+                            <td>{!! $item->name !!}</td>
+                            <td>{!! $item->email !!}</td>
                         </tr>
                         @endforeach
                     </tbody>
                 </table>
+
+                <nav class="text-center">
+                </nav>
+                @else
+                No hay datos!
+                @endif
+            </div>
+        </div>
+    </div>
+</div>

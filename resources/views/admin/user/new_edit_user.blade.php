0f06f6a2ca17a25a8c0c02d4f320fed63d524f51
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2015-10-22 13:50:19 -0500

    ajuste de identacion

--- resources/views/admin/user/new_edit_user.blade.php
+++ resources/views/admin/user/new_edit_user.blade.php
@@ -19,29 +19,29 @@
                     {{-- Fin Mensajes de validaciones del formulario --}}
 
                     @if($user->id)
-                      {!! Form::model($user, ['id' => 'form_user', 'route' => ['admin.user.update', $user->id], 'method' => 'put', 'role'=>'form', 'class'=>'form-horizontal']) !!}
-                      {!! Form::hidden('email_old', $user->email, array('id'=>'email_old'))!!}
+                    {!! Form::model($user, ['id' => 'form_user', 'route' => ['admin.user.update', $user->id], 'method' => 'put', 'role'=>'form', 'class'=>'form-horizontal']) !!}
+                    {!! Form::hidden('email_old', $user->email, array('id'=>'email_old'))!!}
                     @else
-                      {!!Form::model($user, ['id' => 'form_user', 'route' => 'admin.user.store', 'role'=>'form', 'class'=>'form-horizontal']) !!}
+                    {!!Form::model($user, ['id' => 'form_user', 'route' => 'admin.user.store', 'role'=>'form', 'class'=>'form-horizontal']) !!}
                     @endif
 
                     {{--Se valida que si lleguen datos correctos--}}
                     @if (!empty($user))
 
-                        {{-- Campos del formulario --}}
-                        @include ('admin.user.partials.fields')
-                        {{-- Fin Campos del formulario --}}
+                    {{-- Campos del formulario --}}
+                    @include ('admin.user.partials.fields')
+                    {{-- Fin Campos del formulario --}}
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">Guardar</button>
-                            </div>
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary">Guardar</button>
                         </div>
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <a href="{{URL::to('/')}}/admin/user" class="btn btn-info">Volver</a>
-                            </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <a href="{{URL::to('/')}}/admin/user" class="btn btn-info">Volver</a>
                         </div>
+                    </div>
                     @else
                     <p class="">No existe información para este item</p>
                     @endif
@@ -56,16 +56,16 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">Detalles</h3>
-            </div>
+                <div class="panel-heading">
+                    <h3 class="panel-title">Detalles</h3>
+                </div>
                 <div class="panel-body">
                     {!! Form::model($user, ['id' => 'form_user',  'role'=>'form', 'class'=>'form-horizontal']) !!}
                     @if (!empty($user))
 
-                        {{-- Campos del formulario --}}
-                        @include ('admin.user.partials.fields')
-                        {{-- Fin Campos del formulario --}}
+                    {{-- Campos del formulario --}}
+                    @include ('admin.user.partials.fields')
+                    {{-- Fin Campos del formulario --}}
 
                     @else
                     <p class="">No existe información para éste usuario</p>
--- resources/views/admin/user/view_user.blade.php
+++ resources/views/admin/user/view_user.blade.php
@@ -12,68 +12,68 @@
             <div class="controls form-inline">
                 <a href="{!! URL::to('/') !!}/admin/user/create" class="btn btn-primary pull-right">Crear Usuario</a>
                 <div class="input-group">
-                  {!! Form::open(array('url' => 'admin/users/search', 'id' => 'search_form', 'method'=>'GET', 'class'=>'control-group')) !!}
-                  <div class="form-group">
+                    {!! Form::open(array('url' => 'admin/users/search', 'id' => 'search_form', 'method'=>'GET', 'class'=>'control-group')) !!}
+                    <div class="form-group">
                         <input id="search"  name="search"  type="text" required="true" class="form-control" placeholder="Buscar..." value="@if(isset($search)){!! $search !!}@endif" >
-                  </div>
-                  <button class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-                  <a href="{!!URL::to('/')!!}/admin/user" title="Refrescar"class="btn btn-default"><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span></a>
-                  {!! Form::close() !!}
-              </div>
+                    </div>
+                    <button class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                    <a href="{!!URL::to('/')!!}/admin/user" title="Refrescar"class="btn btn-default"><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span></a>
+                    {!! Form::close() !!}
+                </div>
             </div>
-          <div class="table-responsive">
-            @if (count($items) > 0)
+            <div class="table-responsive">
+                @if (count($items) > 0)
 
-            <!--Mensajes-->
-                    @include('admin.alert.messages-success')
-            <!--Fin Mensajes-->
+                <!--Mensajes-->
+                @include('admin.alert.messages-success')
+                <!--Fin Mensajes-->
 
-            <table class="table table-striped">
-            @if($items->isEmpty())
-                <div class="well text-center">No se encontraron registros</div>
-            @else
-                <thead>
-                    <tr>
-                        <th>Acciones</th>
-                        <th>#</th>
-                        <th>Nombre</th>
-                        <th>Usuario</th>
-                        <th>Prefil</th>
-                        <th>Habilitado</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($items as $item)
-                    <tr>
-                        <td style="width: 150px !important">
-                            <table>
-                              <tr>
-                                <td><a title="Detalles" href="{!! URL::to('/') !!}/admin/user/{!! $item->id !!}"><span class="glyphicon glyphicon-eye-open btn btn-default btn-xs"></span></a></td>
-                                <td><a title="Editar" href="{!! URL::to('/') !!}/admin/user/{!! $item->id !!}/edit"><span class="glyphicon glyphicon-pencil btn btn-default btn-xs"></span></a></td>
-                                <td>{!! Form::open(['action' => ['UserController@destroy', $item->id], 'method' => 'delete', 'style' => 'display: inline;']) !!}
-                                    <button title="Eliminar" type="submit" onclick="return Util.confirmDelete(this);" class="glyphicon glyphicon-trash btn btn-default btn-xs"></button>
-                                    {!! Form::close() !!}
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                    <td>{!! $item->id !!}</td>
-                    <td>{!! $item->name !!}</td>
-                    <td>{!! $item->email !!}</td>
-                    <td>{!! $item->profile !!}</td>
-                    <td>{!! $item->enable !!}</td>
-                </tr>
-                @endforeach
-            </tbody>
-            @endif
-        </table>
-        {!! $items->render() !!}
-        @else
-        No hay datos!
-        @endif
+                <table class="table table-striped">
+                    @if($items->isEmpty())
+                    <div class="well text-center">No se encontraron registros</div>
+                    @else
+                    <thead>
+                        <tr>
+                            <th>Acciones</th>
+                            <th>#</th>
+                            <th>Nombre</th>
+                            <th>Usuario</th>
+                            <th>Prefil</th>
+                            <th>Habilitado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($items as $item)
+                        <tr>
+                            <td style="width: 150px !important">
+                                <table>
+                                    <tr>
+                                        <td><a title="Detalles" href="{!! URL::to('/') !!}/admin/user/{!! $item->id !!}"><span class="glyphicon glyphicon-eye-open btn btn-default btn-xs"></span></a></td>
+                                        <td><a title="Editar" href="{!! URL::to('/') !!}/admin/user/{!! $item->id !!}/edit"><span class="glyphicon glyphicon-pencil btn btn-default btn-xs"></span></a></td>
+                                        <td>{!! Form::open(['action' => ['UserController@destroy', $item->id], 'method' => 'delete', 'style' => 'display: inline;']) !!}
+                                            <button title="Eliminar" type="submit" onclick="return Util.confirmDelete(this);" class="glyphicon glyphicon-trash btn btn-default btn-xs"></button>
+                                            {!! Form::close() !!}
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                            <td>{!! $item->id !!}</td>
+                            <td>{!! $item->name !!}</td>
+                            <td>{!! $item->email !!}</td>
+                            <td>{!! $item->profile !!}</td>
+                            <td>{!! $item->enable !!}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    @endif
+                </table>
+                {!! $items->render() !!}
+                @else
+                No hay datos!
+                @endif
+            </div>
+        </div>
     </div>
-</div>
-</div>
 </div>
 @stop
 
